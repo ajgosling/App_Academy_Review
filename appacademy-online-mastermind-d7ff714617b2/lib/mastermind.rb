d8bb85ea01e8a8a617a1d4fdd9557c67ee225016
AJ Gosling <ajgosling2@gmail.com>
2020-01-13 15:38:23 -0800

    working match checking

--- appacademy-online-mastermind-d7ff714617b2/lib/mastermind.rb
+++ appacademy-online-mastermind-d7ff714617b2/lib/mastermind.rb
@@ -3,7 +3,7 @@ class Code
   attr_reader :pegs
 
   PEGS = ["R", "G", "B", "Y", "O", "P"]
-  def initialize(pegs = Code.random)
+  def initialize
     @pegs = pegs
   end
 
@@ -13,22 +13,31 @@ class Code
       str += PEGS.sample
     end
 
-    return str
+    return Code.new(str)
   end
 
   def exact_matches(other)
-
+    count = 0
+    self.pegs.each_char.with_index do |c, i|
+      count += 1 if c == other.pegs[i]
+    end
+    return count
   end
 
   def near_matches(other)
+    count = 0
+    PEGS.each do |color|
+      count += [self.pegs.count(color), other.pegs.count(color)].min
+    end
+
+    return count - self.exact_matches(other)
   end
 end
 
 class Game
   attr_reader :secret_code
+
+  def initialize(secret_code = Code.new)
+
+  end
 end
-
-c = Code.new
-
-new_str = "cccba".split("")
-p "abccc".split("").count {|c, i| c == new_str[0]}
