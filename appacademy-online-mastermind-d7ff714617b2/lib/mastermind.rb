6d6dc109477fde125123d98718ec98b51e13c33d
AJ Gosling <ajgosling2@gmail.com>
2020-01-12 17:01:05 -0800

    starting mastermind

--- appacademy-online-mastermind-d7ff714617b2/lib/mastermind.rb
+++ appacademy-online-mastermind-d7ff714617b2/lib/mastermind.rb
@@ -1,7 +1,20 @@
+# require 'byebug'
 class Code
   attr_reader :pegs
+
+  # def random
 end
 
 class Game
   attr_reader :secret_code
 end
+
+class Array
+  def average
+
+    return self.sum / self.length.to_f
+  end
+
+end
+
+p [1, 2, 3, 4].average
