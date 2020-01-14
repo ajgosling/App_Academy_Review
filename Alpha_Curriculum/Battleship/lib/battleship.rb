db4e2553bb7f670f0aa6950e9b849e03cfd29365
AJ Gosling <ajgosling2@gmail.com>
2020-01-14 01:52:26 -0800

    board class mostly implemented

--- Alpha_Curriculum/Battleship/lib/battleship.rb
+++ Alpha_Curriculum/Battleship/lib/battleship.rb
@@ -1,3 +1,34 @@
+require_relative 'board.rb'
+require_relative 'player.rb'
+
 class BattleshipGame
   attr_reader :board, :player
+
+  def initialize(player, board)
+    @player = player
+    @board = board
+  end
+
+  def attack(pos)
+    @board[pos] = :x
+  end
+
+  def count
+    return @board.count
+  end
+
+  def game_over?
+    return @board.won?
+  end
+
+  # def play_turn
+  #   # need to curate string into position
+
+  #   pos = gets.chomp.split(",")
+  #   self.attack(pos)
+  # end
 end
+
+# b = Board.new
+
+# b.display
--- Alpha_Curriculum/Battleship/lib/board.rb
+++ Alpha_Curriculum/Battleship/lib/board.rb
@@ -1,2 +1,56 @@
 class Board
+	attr_accessor :grid
+
+  def initialize(grid = Board.default_grid)
+    @grid = grid
+  end
+
+  def self.default_grid
+    return Array.new(10) {Array.new(10, nil)}
+  end
+
+  def [](pos)
+    return @grid[pos.first][pos.last]
+  end
+
+	def []=(pos, new_val)
+		@grid[pos.first][pos.last] = new_val
+  end
+
+  def display
+    @grid.each do |row|
+      print row
+    end
+    return true
+  end
+
+  def count
+    total = 0
+    @grid.each do |row|
+        total += row.count(:s)
+    end
+    return total
+	end
+
+	def empty?(pos = nil)
+		return self[pos].nil? if pos
+
+		# if not explicitly passed pos, check whole board
+		return self.count == 0
+	end
+
+	def full?
+		@grid.flatten.none? {|el| el.nil?}
+	end
+
+	def won?
+		@grid.flatten.none? {|el| el == :s}
+	end
+
 end
+
+b = Board.new
+p b[[1, 2]]
+
+b[[1, 2]] = :x
+p b[[1, 2]]
