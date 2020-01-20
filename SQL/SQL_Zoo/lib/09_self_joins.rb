3fbbfc0c8d1785ae87cfe53477cc37a29687bc79
AJ Gosling <ajgosling2@gmail.com>
2020-01-20 14:37:48 -0800

    finish 08

--- SQL/SQL_Zoo/lib/09_self_joins.rb
+++ SQL/SQL_Zoo/lib/09_self_joins.rb
@@ -17,12 +17,22 @@ require_relative './sqlzoo.rb'
 def num_stops
   # How many stops are in the database?
   execute(<<-SQL)
+    SELECT
+      COUNT(*)
+    FROM
+      stops
   SQL
 end
 
 def craiglockhart_id
   # Find the id value for the stop 'Craiglockhart'.
   execute(<<-SQL)
+    SELECT
+      id
+    FROM
+      stops
+    WHERE
+      name = 'Craiglockhart'
   SQL
 end
 
@@ -51,6 +61,18 @@ def connecting_routes
   # that link these stops have a count of 2. Add a HAVING clause to restrict
   # the output to these two routes.
   execute(<<-SQL)
+    SELECT
+      company,
+      num,
+      COUNT(*)
+    FROM
+      routes
+    WHERE
+      stop_id = 149 OR stop_id = 53
+    GROUP BY
+      company, num
+    HAVING
+      COUNT(*) = 2
   SQL
 end
 
