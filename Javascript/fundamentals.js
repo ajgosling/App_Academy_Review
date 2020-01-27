3d0320287304cbfc8d00d3302c75d43381a66e6a
AJ Gosling <ajgosling2@gmail.com>
2020-01-26 21:56:55 -0800

    day 2 of fundamentals

--- Javascript/fundamentals.js
+++ Javascript/fundamentals.js
@@ -147,3 +147,34 @@ function titleize(names, cb) {
 // Elephant.prototype.play: print out a random trick, e.g. "(name) is (trick)!"
 // Hint: look up some JS Math methods!
 // Make sure to create an elephant and test all of these functions out on them method style!
+
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+    this.height += 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+    this.tricks.push(trick)
+};
+
+Elephant.prototype.play = function () {
+    let randomIndex = Math.floor(Math.random() * this.tricks.length);
+    let randomTrick = this.tricks[randomIndex];
+    console.log(`${this.name} is ${randomTrick}!`);
+};
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
