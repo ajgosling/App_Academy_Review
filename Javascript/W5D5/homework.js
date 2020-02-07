65499074b05e952e80af1edb699abaa6913e446a
AJ Gosling <ajgosling2@gmail.com>
2020-02-06 23:54:48 -0800

    working on hw

--- Javascript/W5D5/homework.js
+++ Javascript/W5D5/homework.js
@@ -1,4 +1,10 @@
 function alertCb() {
     alert('Hammertime');
 }
-setTimeout(alertCb, 5000);
+// setTimeout(alertCb, 5000);
+
+function hammerTime(time) {
+    window.setTimeout(() => alert(`${time} is hammerTime`), time);
+}
+
+hammerTime(5000);
