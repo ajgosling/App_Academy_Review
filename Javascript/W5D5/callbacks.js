e86233905cc3e07ab551909dd337428dc63f445e
AJ Gosling <ajgosling2@gmail.com>
2020-02-07 01:21:01 -0800

    addNumbers with callbacks

--- Javascript/W5D5/callbacks.js
+++ Javascript/W5D5/callbacks.js
@@ -45,4 +45,28 @@ class Clock {
     }
 }
 
-const clock = new Clock();
+// const clock = new Clock();
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question("give num: ", (res) => {
+            sum += parseInt(res);
+            console.log(`sum is ${sum}`);
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        });
+    } else {
+        completionCallback(sum);
+    }
+}
+
+addNumbers(0, 3, sum => {
+    console.log(`Total Sum: ${sum}`);
+    reader.close();
+});
