2584ce4d7ddc5dab1a9d24945b4cc4d357b2e4eb
AJ Gosling <ajgosling2@gmail.com>
2020-02-07 00:10:30 -0800

    finished w5d5 hw

--- Javascript/W5D5/homework.js
+++ Javascript/W5D5/homework.js
@@ -9,20 +9,20 @@
 
 // hammerTime(5000);
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const reader = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-reader.question("would you like some tea?", (res1) => {
-    console.log(res1);
-    reader.question("would you like some biscuits?", (res2) => {
-        console.log(`so you ${res1} want tea and you ${res2} want coffee`);
-        reader.close();
-    });
-});
+// reader.question("would you like some tea?", (res1) => {
+//     console.log(res1);
+//     reader.question("would you like some biscuits?", (res2) => {
+//         console.log(`so you ${res1} want tea and you ${res2} want coffee`);
+//         reader.close();
+//     });
+// });
 
 function Cat() {
     this.name = 'Markov';
@@ -40,3 +40,7 @@ Dog.prototype.chase = function (cat) {
 
 const Markov = new Cat();
 const Noodles = new Dog();
+
+Noodles.chase(Markov);
+Noodles.chase.call(Markov, Noodles);
+Noodles.chase.apply(Markov, [Noodles]);
