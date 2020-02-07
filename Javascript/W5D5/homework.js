b36259c105d456b1ff6cf8a5b2406db397867e73
AJ Gosling <ajgosling2@gmail.com>
2020-02-07 00:00:36 -0800

    next part of hw

--- Javascript/W5D5/homework.js
+++ Javascript/W5D5/homework.js
@@ -1,10 +1,42 @@
-function alertCb() {
-    alert('Hammertime');
-}
-// setTimeout(alertCb, 5000);
+// function alertCb() {
+//     alert('Hammertime');
+// }
+// // setTimeout(alertCb, 5000);
 
-function hammerTime(time) {
-    window.setTimeout(() => alert(`${time} is hammerTime`), time);
+// function hammerTime(time) {
+//     window.setTimeout(() => alert(`${time} is hammerTime`), time);
+// }
+
+// hammerTime(5000);
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.question("would you like some tea?", (res1) => {
+    console.log(res1);
+    reader.question("would you like some biscuits?", (res2) => {
+        console.log(`so you ${res1} want tea and you ${res2} want coffee`);
+        reader.close();
+    });
+});
+
+function Cat() {
+    this.name = 'Markov';
+    this.age = 3;
 }
 
-hammerTime(5000);
+function Dog() {
+    this.name = 'Noodles';
+    this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+    console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat();
+const Noodles = new Dog();
