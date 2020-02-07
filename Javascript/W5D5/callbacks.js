0d1229620ebe980c45687bb53e3645f0ee9d1601
AJ Gosling <ajgosling2@gmail.com>
2020-02-07 01:13:09 -0800

    new clock

--- Javascript/W5D5/callbacks.js
+++ Javascript/W5D5/callbacks.js
@@ -1,16 +1,48 @@
-const readline = require('readline');
+class Clock {
+    constructor() {
+        // 1. Create a Date object.
+        let newDate = new Date;
 
-const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+        // 2. Store the hours, minutes, and seconds.
+        this.hours = newDate.getHours();
+        this.minutes = newDate.getMinutes();
+        this.seconds = newDate.getSeconds();
 
-reader.question("What is your name?", function (answer) {
-    console.log(`Hello ${answer}!`);
+        // 3. Call printTime.
+        this.printTime();
 
-    // Close the reader, which will allow the program to end because it
-    // is no longer waiting for any events.
-    reader.close();
-});
+        // 4. Schedule the tick at 1 second intervals.
+        setInterval(() => this._tick.call(this), 1000);
+    }
 
-console.log("Last program line");
+    printTime() {
+        // Format the time in HH:MM:SS
+        // Use console.log to print it.
+        let hoursString = this.hours < 10 ? `0${this.hours}` : this.hours.toString();
+        let minutesString = this.minutes < 10 ? `0${this.minutes}` : this.minutes.toString();
+        let secondsString = this.seconds < 10 ? `0${this.seconds}` : this.seconds.toString();
+
+        console.log(`${hoursString}:${minutesString}:${secondsString}`);
+    }
+
+    _tick() {
+        // 1. Increment the time by one second.
+        // 2. Call printTime.
+        this.seconds += 1;
+        if (this.seconds >= 60) {
+            this.minutes += 1;
+            this.seconds = 0;
+        }
+        if (this.minutes >= 60) {
+            this.hours += 1;
+            this.minutes = 0;
+        }
+        if (this.hours >= 12) {
+            this.hours = 0;
+        }
+        this.printTime();
+
+    }
+}
+
+const clock = new Clock();
