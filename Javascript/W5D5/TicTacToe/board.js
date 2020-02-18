be2b004213c5107fe3d4b83cd160cdc4c3ad9fc0
AJ Gosling <ajgosling2@gmail.com>
2020-02-17 16:15:35 -0800

    putting tic tac toe on the shelf

---
+++ Javascript/W5D5/TicTacToe/board.js
@@ -0,0 +1,20 @@
+
+class Board {
+    constructor(color) {
+        this.color = color;
+    }
+
+    won() {
+    }
+
+    winner() {
+    }
+
+    empty(pos) {
+    }
+
+    place_mark(pos, mark) {
+    }
+}
+
+module.exports = Board;
