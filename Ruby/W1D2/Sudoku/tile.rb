9a51bb928f172b89478fc8514d38e3c634531299
AJ Gosling <ajgosling2@gmail.com>
2020-04-08 20:40:13 -0700

    finish tile class

--- Ruby/W1D2/Sudoku/tile.rb
+++ Ruby/W1D2/Sudoku/tile.rb
@@ -4,7 +4,6 @@ class Tile
     def initialize(value, given)
         @value = value
         @given = given
-
     end
 
     def to_s
@@ -14,6 +13,12 @@ class Tile
             return @value.to_s
         else
             return " "
+        end
+    end
+
+    def value=(value)
+        unless @given
+            @value = value
         end
     end
 end
