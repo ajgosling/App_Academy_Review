a642ceb8bae83acccab8e26185089b2eff6e4696
AJ Gosling <ajgosling2@gmail.com>
2020-04-09 21:32:53 -0700

    file string working

--- Ruby/W1D2/Sudoku/board.rb
+++ Ruby/W1D2/Sudoku/board.rb
@@ -1,10 +1,19 @@
 require_relative 'tile.rb'
 
 class Board
-    def initialize(grid)
-        @grid = grid
+    def initialize(file_string = "./puzzles/sudoku1.txt")
+        @grid = Board.from_file(file_string)
+    end
+
+    def Board.from_file(file_string)
+        file = File.new(file_string)
+        return file.readlines.map do |line|
+            p line.chomp.chars.map(&:to_i)
+        end
     end
 
     def to_s
     end
 end
+
+Board.from_file('./puzzles/sudoku1.txt')
