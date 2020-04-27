37375d8876786daace4d933f35c91c42257f4d7e
AJ Gosling <ajgosling2@gmail.com>
2020-04-26 21:23:12 -0700

    solving columns and rows

--- Ruby/W1D2/Sudoku/board.rb
+++ Ruby/W1D2/Sudoku/board.rb
@@ -19,10 +19,42 @@ class Board
         end
     end
 
-    def to_s
+    def render
+        @grid.each_with_index do |row, i|
+            if i == 3 || i == 6
+                puts "- - - - - - - - -"
+            end
+            puts Board.format_row(row)
+        end
+    end
+
+    def solved?
+
+    end
+
+    def solved_boxes?
+
+    end
+
+    def solved_columns?
+        return @grid.transpose.all? {|row| valid_set?(row)}
+    end
+
+    def solved_rows?
+        return @grid.all? {|row| valid_set?(row)}
+    end
+
+    def valid_set?(arr)
+        return false unless arr.all? {|el| el >= 1 && el <= 9}
+        return arr.uniq.length == 9
+    end
+
+    def Board.format_row(arr)
+        return "#{arr[0..2].join(" ")}|#{arr[0..2].join(" ")}|#{arr[0..2].join(" ")}"
+
     end
 end
 
 b = Board.new
-
-puts b.grid.first
+# p Board.format_row([1, 2, 3, 4, 5, 6, 7, 8, 9])
+b.render
