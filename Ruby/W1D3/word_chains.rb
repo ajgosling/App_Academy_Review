09c7ee51cd6f7a26f367fd6c93a403ddd045d3b6
AJ Gosling <ajgosling2@gmail.com>
2020-05-04 17:07:49 -0700

    starting word_chains

---
+++ Ruby/W1D3/word_chains.rb
@@ -0,0 +1,9 @@
+class WordChainer
+    def initialize(dictionary_file_name)
+        file = File.open(dictionary_file_name)
+        file.readlines[0..5].each do |line|
+        end
+
+    end
+
+end
