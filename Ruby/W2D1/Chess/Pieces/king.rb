ac06890c87f7b3deba86bf7b7e8f6a46b274c89d
AJ Gosling <ajgosling2@gmail.com>
2020-06-11 16:01:21 -0700

    implementing stepable

--- Ruby/W2D1/Chess/Pieces/king.rb
+++ Ruby/W2D1/Chess/Pieces/king.rb
@@ -3,4 +3,17 @@ require_relative 'stepable'
 class King < Piece
 
     # pro
+
+    def move_diffs
+        [
+            [0, 1],
+            [0, -1],
+            [1, 0],
+            [-1, 0],
+            [1, 1],
+            [1, -1],
+            [-1, 1],
+            [-1, -1]
+        ]
+    end
 end
--- Ruby/W2D1/Chess/Pieces/stepable.rb
+++ Ruby/W2D1/Chess/Pieces/stepable.rb
@@ -1,3 +1,12 @@
 module Stepable
+    def moves
+        # iterate over move diffs, change pos according to move dif
+        # make sure result is a valid move
+        moves_arr = []
+        move_diffs.each do |diff|
+            moves_arr << [@pos.first + diff.first, @pos.last + diff.last]
+        end
 
+        return moves_arr
+    end
 end
