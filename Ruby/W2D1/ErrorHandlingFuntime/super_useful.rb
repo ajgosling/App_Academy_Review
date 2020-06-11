5d97ef38240fb29eae240e131bc9939ac972d9a6
AJ Gosling <ajgosling2@gmail.com>
2020-06-10 19:56:40 -0700

    finish error testing

--- Ruby/W2D1/ErrorHandlingFuntime/super_useful.rb
+++ Ruby/W2D1/ErrorHandlingFuntime/super_useful.rb
@@ -15,24 +15,55 @@ end
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
+  if FRUITS.include?(maybe_fruit)
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
   else
-    raise StandardError
+    raise OtherFruitError
   end
 end
 
 def feed_me_a_fruit
-  puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit)
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+
+    reaction(maybe_fruit)
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue OtherFruitError => e
+    puts e.message
+  end
+
+
+end
+
+class CoffeeError < StandardError
+  def message
+    "Coffee? try again"
+  end
+end
+
+class OtherFruitError < StandardError
+  def message
+    "That's not a fruit! what the heck!"
+  end
 end
 
 # PHASE 4
+
+class FriendError < StandardError
+  def message
+    "that doesn't sound like best friendship to me!"
+  end
+end
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise FriendError if yrs_known < 5
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
