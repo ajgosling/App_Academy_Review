4244d0aac75a3b19752401ebce85f1cdca9e0237
AJ Gosling <ajgosling2@gmail.com>
2020-08-12 22:43:10 -0700

    working on int set

--- Ruby/W2D5_LRUCache/LRU_Cache/lib/p01_int_set.rb
+++ Ruby/W2D5_LRUCache/LRU_Cache/lib/p01_int_set.rb
@@ -1,8 +1,15 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max + 1, false)
   end
-
+# Each index in the @store will correspond to an item, and the value at that index will correspond to its presence (either true or false)
+# e.g., the set { 0, 2, 3 } will be stored as: [true, false, true, true]
+# The size of the array will remain constant!
+# The MaxIntSet should raise an error if someone tries to insert, remove, or check inclusion of a number that is out of bounds.
   def insert(num)
+    raise unless is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
@@ -14,6 +21,7 @@ class MaxIntSet
   private
 
   def is_valid?(num)
+    num >= 0 && num <= max
   end
 
   def validate!(num)
@@ -46,33 +54,41 @@ class IntSet
   end
 end
 
-class ResizingIntSet
-  attr_reader :count
 
-  def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
-    @count = 0
-  end
+m = MaxIntSet.new(4)
+m.insert(0)
+m.insert(2)
+m.insert(3)
+p m
 
-  def insert(num)
-  end
 
-  def remove(num)
-  end
+# class ResizingIntSet
+#   attr_reader :count
 
-  def include?(num)
-  end
+#   def initialize(num_buckets = 20)
+#     @store = Array.new(num_buckets) { Array.new }
+#     @count = 0
+#   end
 
-  private
+#   def insert(num)
+#   end
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
-  end
+#   def remove(num)
+#   end
 
-  def num_buckets
-    @store.length
-  end
+#   def include?(num)
+#   end
 
-  def resize!
-  end
-end
+#   private
+
+#   def [](num)
+#     # optional but useful; return the bucket corresponding to `num`
+#   end
+
+#   def num_buckets
+#     @store.length
+#   end
+
+#   def resize!
+#   end
+# end
