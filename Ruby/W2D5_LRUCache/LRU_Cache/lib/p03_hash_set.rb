1578f1b85c9f9744eb38ad95e859902deeb8a26c
AJ Gosling <ajgosling2@gmail.com>
2020-08-13 18:35:04 -0700

    finish hash set

--- Ruby/W2D5_LRUCache/LRU_Cache/lib/p03_hash_set.rb
+++ Ruby/W2D5_LRUCache/LRU_Cache/lib/p03_hash_set.rb
@@ -7,18 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    return if include?(key)
+
+    @count += 1
+
+    resize! if @count == num_buckets
+
+    self[key.hash] << key
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    @count -= 1 if self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,12 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+
+    @store.flatten.each do |key|
+      insert(key)
+    end
+
+    @store = new_store
   end
 end
