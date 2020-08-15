7fe9fc98b3fa5018400e11bdec68c139d78d1f81
AJ Gosling <ajgosling2@gmail.com>
2020-08-14 23:37:35 -0700

    finish dynamic array

--- Ruby/W2D5_LRUCache/LRU_Cache/lib/p07_dynamic_array_bonus.rb
+++ Ruby/W2D5_LRUCache/LRU_Cache/lib/p07_dynamic_array_bonus.rb
@@ -26,11 +26,10 @@ class StaticArray
   end
 end
 
-s = StaticArray.new(4)
-
-s[4]
 class DynamicArray
   attr_accessor :count
+
+  include Enumerable
 
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
@@ -38,9 +37,19 @@ class DynamicArray
   end
 
   def [](i)
+    return nil if i > @count - 1 || i < -@count
+    return @store[@count + i] if i < 0
+    return @store[i]
   end
 
   def []=(i, val)
+    raise "index too small" if i < -@count
+    return @store[@count + i] = val if i < 0
+    return @store[i] = val if i < @count
+    # if count is 4 and i is 6, need i4 = nil and i5 = nil (i6 = val)
+    (i - @count).times {self.push(nil)}
+    self.push(val)
+    return val
   end
 
   def capacity
@@ -48,27 +57,66 @@ class DynamicArray
   end
 
   def include?(val)
+    any? {|el| el == val}
   end
 
   def push(val)
+    resize! if @count == capacity
+
+    @store[@count] = val
+    @count += 1
+
+    @store
   end
 
   def unshift(val)
+    resize! if @count == capacity
+
+    @count.times do |time|
+      @store[@count - time] = @store[@count - time - 1]
+    end
+
+    @count += 1
+    @store[0] = val
   end
 
   def pop
+    return nil if @count == 0
+    @count -= 1
+    val = @store[@count]
+    @store[@count] = nil
+    return val
   end
 
   def shift
+    return nil if @count == 0
+
+    @count -= 1
+
+    val = @store[0]
+    @count.times do |time|
+      @store[time] = @store[time + 1]
+    end
+
+    @store[@count] = nil
+
+    val
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[@count - 1]
   end
 
   def each
+    i = 0
+    while i < @count
+      yield @store[i]
+      i += 1
+    end
   end
 
   def to_s
@@ -77,8 +125,13 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    return false unless self.length == other.length
+    each_with_index do |el, i|
+      return false unless el == other[i]
+    end
+    true
   end
+
 
   alias_method :<<, :push
   [:length, :size].each { |method| alias_method method, :count }
@@ -86,5 +139,11 @@ class DynamicArray
   private
 
   def resize!
+    new_store = StaticArray.new(capacity * 2)
+    each_with_index do |el, i|
+      new_store[i] = el
+    end
+
+    @store = new_store
   end
 end
