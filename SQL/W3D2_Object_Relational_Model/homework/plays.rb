a545a6cbf48fd0705a4f010e751998cbd570e851
AJ Gosling <ajgosling2@gmail.com>
2020-08-24 14:31:15 -0700

    writing playwrights class

--- SQL/W3D2_Object_Relational_Model/homework/plays.rb
+++ SQL/W3D2_Object_Relational_Model/homework/plays.rb
@@ -124,12 +124,21 @@ class Playwright
     # set instance to have the id that was last inserted
     self.id = PlayDBConnection.instance.last_insert_row_id
   end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        plays
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
 end
 
-p PlayDBConnection.instance.methods
-p PlayDBConnection.instance.foreign_key_list(plays)
-
-# p Play.find_by_title("All My Sons")
 # p Playwright.all
 # p Play.find_by_playwright("Eugene O'Neill")
-# p Playwright.find_by_name("Arthur Miller")
+a = Playwright.find_by_name("Arthur Miller")
+p a
