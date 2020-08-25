b60f6d91fdca7684b4372e1ea946d3d2b62b6d86
AJ Gosling <ajgosling2@gmail.com>
2020-08-24 22:22:49 -0700

    making progress on questions classes

--- SQL/W3D2_Object_Relational_Model/AAQuestions/questions.rb
+++ SQL/W3D2_Object_Relational_Model/AAQuestions/questions.rb
@@ -28,7 +28,6 @@ class User
   end
 
   def self.find_by_id(id)
-
     data = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT
         *
@@ -37,9 +36,107 @@ class User
       WHERE
         id = ?
     SQL
-
+    return nil unless data.length > 0
     User.new(data.first)
   end
 
 end
-p User.find_by_id(1)
+
+class Question
+  attr_accessor :id, :title, :body, :author_id
+
+  def self.all
+    data = QuestionDBConnection.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+    return nil unless data.length > 0
+    Question.new(data.first)
+  end
+
+end
+
+class QuestionFollow
+  attr_accessor :id, :user_id, :question_id
+
+  def self.all
+    data = QuestionDBConnection.instance.execute("SELECT * FROM question_follows")
+    data.map { |datum| QuestionFollow.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+    return nil unless data.length > 0
+    QuestionFollow.new(data.first)
+  end
+
+end
+
+class Reply
+  attr_accessor :id, :body, :question_id, :parent_id, :user_id
+
+  def self.all
+    data = QuestionDBConnection.instance.execute("SELECT * FROM replies")
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @body = options['body']
+    @parent_id = options['parent_id']
+    @user_id = options['user_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+    return nil unless data.length > 0
+    Reply.new(data.first)
+  end
+
+end
+
+# AJ NOTE FOR TOMORROW:
+# COPYING THIS FORMAT FOR EACH TABLE IN Database
+# PARENT_ID FOR ALL REPLIES IS NIL, NOT SURE WHY
+
+p Reply.all
+puts
+puts
+p Reply.find_by_id(3)
