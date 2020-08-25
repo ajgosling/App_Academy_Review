0dc103f220bdcfe611c64ccdf1bdbb8c2e6066e2
AJ Gosling <ajgosling2@gmail.com>
2020-08-24 22:06:23 -0700

    making progress on AAQuestions

---
+++ SQL/W3D2_Object_Relational_Model/AAQuestions/questions.rb
@@ -0,0 +1,31 @@
+require 'sqlite3'
+require 'singleton'
+
+
+
+class QuestionDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class User
+  attr_accessor :id, :fname, :lname
+
+  def self.all
+    data = QuestionDBConnection.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+end
+User.all.each {|u| p u.fname}
