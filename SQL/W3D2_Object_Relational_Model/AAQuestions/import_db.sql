4c22c5d75f0d51c37080741dee20f28a4a401754
AJ Gosling <ajgosling2@gmail.com>
2020-08-24 21:11:51 -0700

    filling out seed data

--- SQL/W3D2_Object_Relational_Model/AAQuestions/import_db.sql
+++ SQL/W3D2_Object_Relational_Model/AAQuestions/import_db.sql
@@ -6,6 +6,16 @@ CREATE TABLE users (
     lname TEXT NOT NULL
 )
 
+INSERT INTO
+  users
+    (fname, lname)
+VALUES
+    ('AJ', 'Gosling'),
+    ('Jourdan', 'Lieblich'),
+    ('Alix', 'Lieblich'),
+    ('Dan', 'Gosling'),
+    ('Elliot', 'Grossman'),
+
 DROP TABLE if exists questions;
 
 CREATE TABLE questions (
@@ -17,6 +27,25 @@ CREATE TABLE questions (
     FOREIGN KEY (author_id) REFERENCES users(id)
 )
 
+INSERT INTO
+  questions
+    (title, body, author_id)
+VALUES
+    (
+        'Is Avatar the greatest series ever?',
+        'I was wonder what all of you thought about the avatar series.',
+        (SELECT id FROM users WHERE fname = 'AJ')
+    ),
+    (
+        'Wondering how to install a climbing hangboard?',
+        'I am trying to follow this youtube channel that teaches me how to install a hangboard but I am not quite getting the hang of it',
+        (SELECT id FROM users WHERE fname = 'Jourdan')
+    ),
+    (
+        'Which college should I go to?',
+        'I am really torn between Cal Poly and Hawaii',
+        (SELECT id FROM users WHERE fname = 'Alix')
+    )
 DROP TABLE if exists question_follows;
 
 CREATE TABLE question_follows (
@@ -28,16 +57,33 @@ CREATE TABLE question_follows (
     FOREIGN KEY (question_id) REFERENCES questions(id)
 )
 
-
--- This should support the many-to-many relationship between questions and users (a user can have many questions she is following, and a question can have many followers).
 -- This is an example of a join table; the rows in question_follows are used to join users to questions and vice versa.
 -- Add a replies table.
--- Each reply should contain a reference to the subject question.
--- Each reply should have a reference to its parent reply.
--- Each reply should have a reference to the user who wrote it.
--- Don't forget to keep track of the body of a reply.
--- "Top level" replies don't have any parent, but all replies have a subject question.
--- It's okay for a column to be self referential; a foreign key can point to a primary key in the same table.
+
+DROP TABLE if exists replies;
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_id) REFERENCES replies(id)
+)
 -- Add a question_likes table.
+DROP TABLE if exists likes;
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
 -- Users can like a question.
 -- Have references to the user and the question in this table
+
