824bc3f51b22d22ec52ee4f734046d9b3ec2957f
AJ Gosling <ajgosling2@gmail.com>
2022-01-10 13:57:51 -0800

    finish algorithmic strategies

--- Alpha_Curriculum/algorithmic_strategies_exercise/02_is_sorted.rb
+++ Alpha_Curriculum/algorithmic_strategies_exercise/02_is_sorted.rb
@@ -2,6 +2,15 @@
 # The method should return true if the elements are in increasing order, false otherwise.
 # Do not use the built-in Array#sort in your solution :)
 
+def is_sorted(arr)
+    i = 0
+    while i < arr.length - 1
+        return false if arr[i] > arr[i + 1]
+        i += 1
+    end
+
+    true
+end
 
 p is_sorted([1, 4, 10, 13, 15])       # => true
 p is_sorted([1, 4, 10, 10, 13, 15])   # => true
--- Alpha_Curriculum/algorithmic_strategies_exercise/04_peak_finder.rb
+++ Alpha_Curriculum/algorithmic_strategies_exercise/04_peak_finder.rb
@@ -3,6 +3,27 @@
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
+def peak_finder(arr)
+    peaks_arr = []
+
+    return peaks_arr if arr.length < 2
+
+    peaks_arr << arr[0] if arr[0] > arr[1]
+
+    i = 1
+
+    while i < arr.length - 1
+        if arr[i] > [arr[i - 1], arr[i + 1]].max
+            peaks_arr << arr[i]
+        end
+        i += 1
+    end
+
+    peaks_arr << arr[-1] if arr[-1] > arr[-2]
+
+    return peaks_arr
+end
+
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
