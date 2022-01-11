4614f0576b1c599a0debc213b974756365b483c8
AJ Gosling <ajgosling2@gmail.com>
2022-01-11 12:22:53 -0800

    finish first file in rspec exercises

--- Alpha_Curriculum/rspec_exercise_1/lib/part_1.rb
+++ Alpha_Curriculum/rspec_exercise_1/lib/part_1.rb
@@ -1,2 +1,25 @@
 def average(num1, num2)
+    (num1 + num2).to_f / 2
+end
+
+def average_array(arr)
+    arr.sum.to_f / arr.length
+end
+
+def repeat(str, num=1)
+    str * num
+end
+
+def yell(str)
+    str.upcase + "!"
+end
+
+def alternating_case(str)
+    str.split.map.with_index do |word, idx|
+        if idx.even?
+            word.upcase
+        else
+            word.downcase
+        end
+    end.join(" ")
 end
