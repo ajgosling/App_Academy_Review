4a6ada27f3030d78fb79a3f2a41079d123e04763
AJ Gosling <ajgosling2@gmail.com>
2022-01-13 10:12:36 -0800

    starting procs exercises

--- Alpha_Curriculum/blocks_project/lib/part_2.rb
+++ Alpha_Curriculum/blocks_project/lib/part_2.rb
@@ -5,3 +5,7 @@ end
 def no_valid_url?(arr)
     arr.none? {|url| url.end_with?(".com", ".net", ".io", ".org")}
 end
+
+def any_passing_students?(arr)
+    arr.any? {|student| student[:grades].sum / student[:grades].size >= 75}
+end
