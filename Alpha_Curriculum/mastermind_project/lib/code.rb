a2c49a4ab2ca62c4d2f2138aabbf0c4e15ee0c4b
AJ Gosling <ajgosling2@gmail.com>
2022-01-19 12:52:00 -0800

    finish mastermind project

--- Alpha_Curriculum/mastermind_project/lib/code.rb
+++ Alpha_Curriculum/mastermind_project/lib/code.rb
@@ -6,4 +6,50 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(arr)
+    arr.all? {|peg| POSSIBLE_PEGS.include?(peg.upcase)}
+  end
+
+  def self.random(num)
+    arr = []
+    num.times {arr << POSSIBLE_PEGS.keys.sample}
+    Code.new(arr)
+  end
+
+  def self.from_string(str)
+    Code.new(str.chars)
+  end
+
+  def initialize(arr)
+    raise "invalid pegs" unless Code.valid_pegs?(arr)
+    @pegs = arr.map(&:upcase)
+  end
+
+  def [](i)
+    @pegs[i]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(other_code)
+    count = 0
+    @pegs.each_with_index {|el, i| count += 1 if el == other_code[i]}
+    count
+  end
+
+  def num_near_matches(other_code)
+    count = 0
+    POSSIBLE_PEGS.keys.each do |color|
+      count += [@pegs.count(color), other_code.pegs.count(color)].min
+    end
+    count - num_exact_matches(other_code)
+  end
+
+  def ==(other_code)
+    return @pegs == other_code.pegs
+  end
 end
--- Alpha_Curriculum/mastermind_project/lib/mastermind.rb
+++ Alpha_Curriculum/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,19 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    p "num exact matches: #{@secret_code.num_exact_matches(code)}"
+    p "num near matches: #{@secret_code.num_near_matches(code)}"
+  end
+
+  def ask_user_for_guess
+    p "Enter a code!"
+    user_code = Code.from_string(gets.chomp)
+    print_matches(user_code)
+    @secret_code == user_code
+  end
 end
