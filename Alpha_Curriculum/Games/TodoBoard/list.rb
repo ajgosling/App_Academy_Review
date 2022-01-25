bc0cafb46868371525792f2f8ff7d30f97896e93
AJ Gosling <ajgosling2@gmail.com>
2022-01-25 12:32:40 -0800

    finished list implementation, starting on todo_board

--- Alpha_Curriculum/Games/TodoBoard/list.rb
+++ Alpha_Curriculum/Games/TodoBoard/list.rb
@@ -42,19 +42,66 @@ class List
   end
 
   def print
-    puts "-" * 40
-    puts @label.center(40)
-    puts "-" * 40
-    puts "Index | Item          | Deadline"
-    puts "-" * 40
+    puts "-" * 50
+    puts @label.center(50)
+    puts "-" * 50
+    puts "Index | Item#{" " * 26}| Deadline"
+    puts "-" * 50
+    @items.each_with_index do |i, idx|
+      i_str = idx.to_s.ljust(6)
+      t_str = i.title.ljust(30)
+      d_str = i.deadline
+      puts "#{i_str}| #{t_str}| #{d_str}"
+    end
+    puts "-" * 50
+  end
 
+  def print_full_item(idx)
+    if i = self[idx]
+      puts "-" * 50
+      puts "#{i.title.ljust(25)}#{i.deadline.rjust(25)}"
+      puts "#{i.description}"
+      puts "-" * 50
+    end
+  end
 
+  def print_priority
+    print_full_item(0)
+  end
+
+  def up(idx, amt=1)
+    if valid_index?(idx)
+      [amt, idx].min.times do |time|
+        swap(idx - time, idx - time - 1)
+      end
+      true
+    else
+      false
+    end
+  end
+
+  def down(idx, amt=1)
+    if valid_index?(idx)
+      [amt, size - 1 - idx].min.times do |time|
+        swap(idx + time, idx + time + 1)
+      end
+      true
+    else
+      false
+    end
+  end
+
+  def sort_by_date!
+    @items.sort_by! {|i| i.deadline}
   end
 end
 
 l = List.new("DADS CAR THING")
 l.add_item('Fix login page', '2019-10-22', 'It loads slow.')
-l.add_item('buy a cheeseburger', '2018-02-31', 'yummy!')
-l.swap(0, 1)
-l
+l.add_item('buy a cheeseburger', '2018-02-31', 'yummy! what a tasty burger!')
+l.add_item('fix car', '2020-02-30', 'need to take car to Jiffy Lube')
+l.add_item('replace desk', '2017-11-01', 'upgrade to an electric standing desk')
+l.add_item('drink water', '2019-10-11', 'important to stay hydrated!!!')
+l.print
+l.sort_by_date!
 l.print
---
+++ Alpha_Curriculum/Games/TodoBoard/todo_board.rb
@@ -0,0 +1,10 @@
+class TodoBoard
+  attr_reader :label
+  def initialize(label)
+    @label = label
+  end
+
+  def get_command
+
+  end
+end
