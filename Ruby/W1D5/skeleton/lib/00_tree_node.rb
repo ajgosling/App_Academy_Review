10d0633683caab302671a0642ce540f414d99305
AJ Gosling <ajgosling2@gmail.com>
2022-03-07 14:07:33 -0800

    finish tree node class

--- Ruby/W1D5/skeleton/lib/00_tree_node.rb
+++ Ruby/W1D5/skeleton/lib/00_tree_node.rb
@@ -6,11 +6,44 @@ class PolyTreeNode
     @children = []
   end
 
-  def parent=(parent_node)
-    parent_node.add_child(self) if parent_node
-    @parent.remove_child(self) if @parent
-    @parent = parent_node
+  def dfs(val)
+    return self if @value == val
+    @children.each do |child|
+      res = child.dfs(val)
+      return res if res
+    end
 
+    nil
+  end
+
+  def bfs(val)
+    queue = [self]
+
+    until queue.empty?
+      curr_node = queue.shift
+      return curr_node if curr_node.value == val
+      queue += curr_node.children
+    end
+    nil
+  end
+
+  def inspect
+    @value
+  end
+
+  def parent=(parent_node)
+    return if self.parent == parent_node
+
+    if @parent
+      @parent.children.delete(self)
+    end
+
+    @parent = parent_node
+    if @parent
+      @parent.children << self
+    end
+
+    self
   end
 
   def include?(node)
@@ -23,15 +56,32 @@ class PolyTreeNode
   end
 
   def add_child(node)
-    @children << node unless include?(node)
+    node.parent = self
   end
 
   def remove_child(node)
-    @children.each_with_index do |child, idx|
-      if node == child
-        return @children.delete_at(idx)
-      end
-    end
-    return nil
+    raise "node is not a child" unless include?(node)
+    node.parent = nil
   end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  n1 = PolyTreeNode.new("root1")
+  n2 = PolyTreeNode.new("root2")
+  n3 = PolyTreeNode.new("root3")
+
+  # connect n3 to n1
+  n3.parent = n1
+  # connect n3 to n2
+  n3.parent = n2
+
+  # this should work
+  raise "Bad parent=!" unless n3.parent == n2
+  raise "Bad parent=!" unless n2.children == [n3]
+
+  # this probably doesn't
+  raise "Bad parent=!" unless n1.children == []
+
+  p "no issues!"
 end
